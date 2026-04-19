d69d351cf8b8cdd2bec0364fccc5ffdabbe11643
molor824 <molormargaderdene@gmail.com>
2026-04-20 00:35:12 +0800

    Changes

--- train.py
+++ train.py
@@ -2,14 +2,14 @@ from ultralytics import YOLO
 from model_loader import load_last
 
 def main():
-    try:
-        path = load_last()
-        print(f"Starting from last weight {path}")
-    except:
-        path = "yolo12n.pt"
-        print(f"Starting from new weight {path}")
+    # try:
+    #     path = load_last()
+    #     print(f"Starting from last weight {path}")
+    # except:
+        # path = "yolo12n.pt"
+        # print(f"Starting from new weight {path}")
 
-    model = YOLO(path)
+    model = YOLO("yolo26n.pt")
     model.train(data="data.yaml", epochs=100, imgsz=640, save=True, batch=-1)
 
 if __name__ == "__main__":
